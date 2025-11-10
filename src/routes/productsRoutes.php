f563aa26e6ceb18ecf1f510e63c4aa026089250f
TeoMunoz <teo.munoz.blasco@gmail.com>
2025-11-10 20:21:38 +0100

    GET Products {id} + PUT products/{id} + Update Product + // DELETE /products/{id}

--- src/routes/productsRoutes.php
+++ src/routes/productsRoutes.php
@@ -51,6 +51,113 @@ return function ($app) {
             return $response->withHeader('Content-Type', 'application/json');
         });
 
+        //GET /products/{id}
+        $group->get('/{id}', function ($request, $response, $args) {
+            require_once __DIR__ . '/../config/database.php';
+
+            $db = new Database();
+            $conn = $db->connect();
+
+            $id = $args['id'];
+
+            $stmt = $conn->prepare("SELECT * FROM products WHERE id = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $product = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (!$product) {
+                $response = $response->withStatus(404);
+                $response->getBody()->write(json_encode([
+                'error' => 'Product not found'
+            ]));
+        return $response->withHeader('Content-Type', 'application/json');
+        }
+
+        $response->getBody()->write(json_encode($product));
+        return $response->withHeader('Content-Type', 'application/json');
+        });
+
+        //PUT products/{id}
+        $group->put('/{id}', function ($request, $response, $args) {
+        require_once __DIR__ . '/../config/database.php';
+
+        $db = new Database();
+        $conn = $db->connect();
+
+        $id = $args['id'];
+        $data = $request->getParsedBody();
+
+        // Validate
+        if (!isset($data['name'], $data['price'], $data['stock'])) {
+            $response = $response->withStatus(400);
+            $response->getBody()->write(json_encode([
+                "error" => "Missing fields: name, price, stock"
+            ]));
+            return $response->withHeader('Content-Type', 'application/json');
+    }
+
+        // Product exist
+        $stmt = $conn->prepare("SELECT * FROM products WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        if ($stmt->rowCount() === 0) {
+            $response = $response->withStatus(404);
+            $response->getBody()->write(json_encode([
+                "error" => "Product not found"
+            ]));
+            return $response->withHeader('Content-Type', 'application/json');
+        }
+
+        // Update Product
+        $stmt = $conn->prepare("
+            UPDATE products 
+            SET name = :name, price = :price, stock = :stock
+            WHERE id = :id
+        ");
+
+        $stmt->bindParam(':name', $data['name']);
+        $stmt->bindParam(':price', $data['price']);
+        $stmt->bindParam(':stock', $data['stock']);
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute();
+
+        $response->getBody()->write(json_encode([
+            "message" => "Product updated successfully",
+            "id" => $id
+        ]));
+
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
+    // DELETE /products/{id}
+    $group->delete('/{id}', function ($request, $response, $args) {
+        require_once __DIR__ . '/../config/database.php';
+
+        $db = new Database();
+        $conn = $db->connect();
+
+        $id = intval($args['id']);
+
+        // Exist
+        $check = $conn->prepare("SELECT * FROM products WHERE id = ?");
+        $check->execute([$id]);
+
+        if ($check->rowCount() === 0) {
+            $response->getBody()->write(json_encode(['error' => 'Product not found']));
+            return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
+        }
+
+        // Delete
+        $stmt = $conn->prepare("DELETE FROM products WHERE id = ?");
+        $stmt->execute([$id]);
+
+        $response->getBody()->write(json_encode(['message' => 'Product deleted']));
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
     });
 
 };
